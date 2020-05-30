fdb19d8c810d6263c5a8d05ce7d58993d87612e6
Jan Weidner <jw3126@gmail.com>
2020-05-31 00:33:59 +0200

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 module TestCompositionsBase
-
 using CompositionsBase
 using CompositionsBase: @var_str
 using Test
@@ -19,6 +18,21 @@ end  # if
 
 if VERSION >= v"1.5.0-DEV.302"  # for ⨟
     include("test_julia15.jl")
+end
+
+@testset "all derived from ∘" begin
+    @test compose === ∘
+    @test var"⨟" === opcompose
+
+    struct FreeMagma
+        word
+    end
+    Base.:(∘)(a::FreeMagma, b::FreeMagma) = FreeMagma((a.word, b.word))
+    FM = FreeMagma
+    @test opcompose(FM(1), FM(2)) === compose(FM(2), FM(1)) === FM((2,1))
+    @test opcompose(FM(1), FM(2), FM(3)) === compose(FM(3), FM(2), FM(1)) === FM(((3,2),1))
+    @test_throws MethodError opcompose()
+    @test_throws MethodError compose()
 end
 
 end  # module
